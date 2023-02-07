0d3a0635bdcf586ecb38e66d0429f9384f13a516
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-02-07 21:52:47 +0700

    Initial commit

---
+++ Looper.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace ForeachOrFor;
+
+[MemoryDiagnoser]
+public class Looper
+{
+    private readonly List<int> _list;
+
+    public Looper()
+    {
+        _list = new List<int>();
+        for (var i = 0; i < 500; i++)
+        {
+            _list.Add(i);
+        }
+    }
+
+    [Benchmark]
+    public void LoopingForeach()
+    {
+        foreach (var item in _list)
+        {
+            //do nothing
+        }
+    }
+
+    [Benchmark]
+    public void LoopingFor()
+    {
+        for (var i = 0; i < _list.Count; i++)
+        {
+//do nothing            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+﻿# Foreach vs Loop
+
+This is a simple repository just to show benchmark which one is better use for looping, for or foreach with the same data size.
+
+## Summary
+
+![Image3](https://github.com/vendyp/foreach-or-for/.github/image3.PNG)
+
+## After All
+
+![Image1](https://github.com/vendyp/foreach-or-for/.github/image1.PNG)
+
+## Detail
+
+![Image2](https://github.com/vendyp/foreach-or-for/.github/image2.PNG)
